76bbf6edb9d46c5d5500892baedd3147aeab4205
bigspambot <67501435+bigspambot@users.noreply.github.com>
2024-01-17 12:42:31 +0000

    first commit - nav complete

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+	return (
+		// check flex-start
+		<section className='flexStart flex-col paddings mb-16'>
+			<h1>Categories</h1>
+			<h1>Posts</h1>
+			<h1>LoadMore</h1>
+		</section>
+	);
+};
+
+export default Home;
